e7fde522e0875bc290179bdc2a1dbbff397163ec
ALong202 <147915024+ALong202@users.noreply.github.com>
2023-11-29 08:59:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from streamlit as st
+import pandas as pd
+
 
 st.title('Data visualization')
+st.header('Data visualization')
+
+data_file = st.file_uploader("Choose a csv file", type =(['.csv']))
+if data_file is not None:
+  df = pd.read_csv(data_file)
+  st.dataframe(df)
+  
+    
