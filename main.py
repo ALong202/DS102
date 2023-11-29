4d7c2cc2f50b79dffc22954d33489af0693c3c6b
ALong202 <147915024+ALong202@users.noreply.github.com>
2023-11-29 09:27:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ if data_file is not None:
     buffer = io.StringIO()
     df.info(buf=buffer)
     st.text(buffer.getvalue())
+    
     st.header('Visualize each attribute')
     for col in list(df.columns):
-        fig, ax = plt.subplot()
+        fig, ax = plt.subplots()
+        ax.hist(df[col], bins =20)
         plt.xlable(col)
         plt.ylable('Quantity')
         plt.pyplot(fig)
